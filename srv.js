e3f2eff9471748634478306cf631e06c80b0b0e8
mudcrab2142 <mudcrab2142@gmail.com>
2023-03-31 23:47:34 +0300

    no message

--- srv.js
+++ srv.js
@@ -1,6 +1,7 @@
 const http = require("http");
 
 const host = '0.0.0.0';
+const port = 8080;
 
 const requestListener = function (req, res) {
 	switch (req.url){
@@ -21,5 +22,5 @@ const requestListener = function (req, res) {
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:8000`);
+    console.log(`Server is running on http://${host}:&{port}`);
 });
