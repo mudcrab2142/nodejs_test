9ae6fa0e65760f6ffc843c8ddbf0269247f4c37c
mudcrab2142 <mudcrab2142@gmail.com>
2023-04-01 00:24:59 +0300

    no message

--- srv.js
+++ srv.js
@@ -1,26 +1,35 @@
 const http = require("http");
+const CyclicDB = require('@cyclic.sh/dynamodb')
+const db = CyclicDB(process.env.CYCLIC_DB)
 
 const host = '0.0.0.0';
 const port = 8080;
 
-const requestListener = function (req, res) {
-	switch (req.url){
-		case "/test1":
-            res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
-            res.end(`{"test1": "kek1"}`);
-            break
-		case "/test2":
-            res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
-            res.end(`{"test2": "kek2"}`);
-            break
-	    default:
-			res.writeHead(404, {'Access-Control-Allow-Origin': '*'});
-			break
+const leaderboard = db.collection("leaderboard")
+
+const scoreHandle = function(json, res) {
+	console.log(json)
+	res.writeHead(200, {'Access-Control-Allow-Origin': '*'})
+}
+
+const server = http.createServer(function (req, res) {
+	if(request.method === 'POST') {
+		var jsonString = '';
+
+		req.on('data', function (data) {
+			jsonString += data;
+		});
+
+		req.on('end', function () {
+			if(req.url == '/score'){
+				scoreHandle(jsonString, res)
+			}	
+		});
+
 	}
-};
+};);
 
 
-const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:&{port}`);
 });
