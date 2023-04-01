342a22bf2ca2ebeab9785462da021f40286f03a5
mudcrab2142 <mudcrab2142@gmail.com>
2023-04-01 05:22:22 +0300

    no message

--- srv.js
+++ srv.js
@@ -1,16 +1,47 @@
 const http = require("http");
-const CyclicDB = require('@cyclic.sh/dynamodb')
-const db = CyclicDB(process.env.CYCLIC_DB)
+const Store = require("jfs");
+const db = new Store("data");
+const { 
+  v1: uuidv1,
+  v4: uuidv4,
+} = require('uuid');
 
 const host = '0.0.0.0';
 const port = 8080;
 
-const leaderboard = db.collection("leaderboard")
+const scoreHandler = function(json, res) {
+	try{
+		console.log(json)
+		db.saveSync(uuidv4(), json);
+	//	await s3.putObject({Body: json, Bucket: process.env.BUCKET, Key: uuidv4()}).promise()
+		res.writeHead(200, {'Access-Control-Allow-Origin': '*'})
+		res.end()
+	} catch {
+		res.writeHead(500, {'Access-Control-Allow-Origin': '*'})
+		res.end()
+	}
+}
 
-const scoreHandle = function(json, res) {
-	console.log(json)
-	res.writeHead(200, {'Access-Control-Allow-Origin': '*'})
-	res.end()
+const leaderboardHandler = function(res) {
+	try{
+
+	/*	s3.listObjects({Bucket: process.env.BUCKET}, function(err, data) {
+			if (err) {
+				console.log("Error", err);
+			} else {
+				console.log("Success", JSON.stringify(data.Contents.sort(o => o.Key)); // example
+			}
+		})
+		await s3.putObject({Body: json, Bucket: process.env.BUCKET, Key: uuidv4()}).promise()*/
+
+		var objs = db.allSync()
+		console.log(objs)
+		res.writeHead(200, {'Access-Control-Allow-Origin': '*'})
+		res.end()
+	} catch {
+		res.writeHead(500, {'Access-Control-Allow-Origin': '*'})
+		res.end()
+	}
 }
 
 const server = http.createServer(function (req, res) {
@@ -23,9 +54,13 @@ const server = http.createServer(function (req, res) {
 
 		req.on('end', function () {
 			if(req.url == '/score'){
-				scoreHandle(jsonString, res)
+				scoreHandler(jsonString, res)
 			}	
 		})
+	} else if (req.method === 'GET'){
+		if(req.url == '/leaderboard'){
+			leaderboardHandler(res)
+		}	
 	}
 })
 
